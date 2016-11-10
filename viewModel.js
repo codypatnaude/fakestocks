fc60bfe104f1c478c3635ae11515280f69617b80
codypatnaude <codypatnaude@gmail.com>
2016-11-10 10:03:34 -0500

    Delete viewModel.js

--- viewModel.js
+++
@@ -1,7 +0,0 @@
-function viewModel(){
-  let self = this
-  self.firstName = ko.observable("Bob")
-  self.lastName = ko.observable("Boberino")
-
-  self.nameObj = ko.observable({firstName:"Sara", lastName:"Fortin"})
-}
